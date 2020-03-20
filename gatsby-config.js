4ccf35d9e98dfa47fba481b62acf721bbcb83997
Dries Bos <info@driesbos.com>
2020-03-20 10:05:34 +0100

    chore: sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,41 @@
-let contentfulConfig
+let contentfulConfig;
 
 try {
   // Load the Contentful config from the .contentful.json
-  contentfulConfig = require('./.contentful')
+  contentfulConfig = require("./.contentful");
 } catch (_) {}
 
 // Overwrite the Contentful config with environment variables if they exist
 contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
   accessToken:
-    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
-}
+    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken
+};
 
-const { spaceId, accessToken } = contentfulConfig
+const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
   throw new Error(
-    'Contentful spaceId and the delivery token need to be provided.'
-  )
+    "Contentful spaceId and the delivery token need to be provided."
+  );
 }
 
 module.exports = {
-  pathPrefix: '/gatsby-contentful-starter',
+  pathPrefix: "/gatsby-contentful-starter",
+  siteMetadata: {
+    title: `My Awesome Blog`,
+    description: `An awesome blog displaying my awesome posts.`,
+    author: `YOUR_NAME`,
+    siteUrl: `https://www.example.com`
+  },
   plugins: [
-    'gatsby-transformer-remark',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-remark",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sitemap",
     {
-      resolve: 'gatsby-source-contentful',
-      options: contentfulConfig,
-    },
-  ],
-}
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig
+    }
+  ]
+};
